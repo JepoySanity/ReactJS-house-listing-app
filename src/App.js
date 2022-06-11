a4d94f60c8632f7d7f7f003705dd8d82df2a35bf
Jepoy Factoran <factoran20@gmail.com>
2022-06-11 20:55:59 +0800

    prepare initial routes for the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot_password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar goes here.... */}
     </>
   );
 }
