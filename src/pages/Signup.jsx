5a659078cf280bfceb0b2e373c58c15c870037de
Jepoy Factoran <factoran20@gmail.com>
2022-06-11 22:10:22 +0800

    added sigin and singup components

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,81 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 function Signup() {
+  const [showPass, setShowPass] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+  const navigate = useNavigate();
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Register Now</p>
+        </header>
+        <main>
+          <form>
+            <input
+              type="test"
+              className="nameInput"
+              placeholder="Your Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPass ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() =>
+                  setShowPass((prevState) => {
+                    setShowPass(!prevState);
+                  })
+                }
+              />
+            </div>
+            <div className="signUpBar">
+              <p className="signUpText">Sign Up</p>
+              <button className="signUpButton">
+                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+          <Link to="/signin" className="registerLink">
+            Sign In instead
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
 
