a026b2faee4d0678603b566cf9df52346efb90c4
Jepoy Factoran <factoran20@gmail.com>
2022-06-12 07:37:55 +0800

    save user data as doc to firestore

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
@@ -34,10 +35,16 @@ function Signup() {
         email,
         password
       );
+      //get user credentials
       const user = userCredential.user;
       updateProfile(auth.currentUser, {
         displayName: name,
       });
+      //crete new doc/record on firestore
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
       navigate("/");
     } catch (error) {
