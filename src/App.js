983d05ac8e77719f876bb4c65d51f2b8a336c70c
Jepoy Factoran <factoran20@gmail.com>
2022-06-12 08:10:23 +0800

    implement react toastify for error and success pop up messages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
@@ -22,6 +24,7 @@ function App() {
         {/* Navbar goes here.... */}
         <Navbar />
       </Router>
+      <ToastContainer />
     </>
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -48,7 +49,7 @@ function Signup() {
 
       navigate("/");
     } catch (error) {
-      console.log(error);
+      toast.error("Ooooops! something went wrong");
     }
   };
 
